085cdca00c675ce5c7b2302c3675eff2bae9eed6
Jamova01 <jorgemova01@gmail.com>
2024-07-16 18:53:20 -0500

    Added route information session and zoom buttons to map in Home component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,6 +11,15 @@ import useOfflineSearch from "../../hooks/useOfflineSearch";
 import useSelectedLocation from "../../hooks/useSelectedLocation";
 import useWayfinding from "../../hooks/useWayfinding";
 
+import {
+  ArrowTurnUpLeftIcon,
+  ArrowTurnUpRightIcon,
+  MapPinIcon,
+  MinusIcon,
+  PlusIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+
 function Home() {
   const options = useMemo<TGetVenueOptions>(
     () => ({
@@ -31,6 +40,8 @@ function Home() {
     setStartLocation,
     setEndLocation,
     clearAll,
+    distance,
+    steps,
   } = useWayfinding(mapView);
 
   const [searchQueryStart, setSearchQueryStart] = useState("");
@@ -145,9 +156,9 @@ function Home() {
   }, [mapView, venue]);
 
   return (
-    <section className="flex flex-col h-screen">
-      <nav className="bg-gray-800 text-white p-4 flex justify-between items-center">
-        <div>
+    <section className="flex flex-col h-screen bg-gray-900 text-white">
+      <nav className="bg-gray-800 p-4 flex justify-between items-center h-16">
+        <div className="flex items-center">
           <select
             ref={mapGroupSelectRef}
             className="bg-gray-700 text-white p-2 rounded mr-2"
@@ -171,28 +182,23 @@ function Home() {
         </div>
         <button
           onClick={clearAll}
-          className="bg-red-500 text-white p-2 rounded"
+          className="flex items-center bg-red-500 text-white p-2 rounded"
         >
-          Clear All
+          <TrashIcon className="w-5 h-5 mr-2" />
+          Clear Route
         </button>
       </nav>
 
-      <div className="flex-1 flex">
-        <aside className="bg-gray-100 w-64 p-4 overflow-y-auto">
+      <div className="flex flex-1 overflow-hidden">
+        <aside className="bg-gray-800 w-64 p-4 overflow-y-auto">
           <div className="mb-4">
             <label className="block mb-2">Start Location</label>
             <div className="flex">
               <input
                 value={startLocation?.name || ""}
                 readOnly
-                className="w-full p-2 border border-gray-300 rounded"
+                className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded"
               />
-              <button
-                onClick={() => setStartLocation(undefined)}
-                className="bg-red-500 text-white p-2 rounded ml-2"
-              >
-                X
-              </button>
             </div>
             <input
               type="text"
@@ -201,7 +207,7 @@ function Home() {
               onChange={(e) => {
                 setSearchQueryStart(e.target.value);
               }}
-              className="w-full p-2 mt-2 border border-gray-300 rounded"
+              className="w-full p-2 mt-2 bg-gray-700 text-white border border-gray-600 rounded"
             />
             <div className="search-results mt-2">{searchResultsStart}</div>
           </div>
@@ -212,14 +218,8 @@ function Home() {
               <input
                 value={endLocation?.name || ""}
                 readOnly
-                className="w-full p-2 border border-gray-300 rounded"
+                className="w-full p-2 bg-gray-700 text-white border border-gray-600 rounded"
               />
-              <button
-                onClick={() => setEndLocation(undefined)}
-                className="bg-red-500 text-white p-2 rounded ml-2"
-              >
-                X
-              </button>
             </div>
             <input
               type="text"
@@ -228,14 +228,76 @@ function Home() {
               onChange={(e) => {
                 setSearchQueryEnd(e.target.value);
               }}
-              className="w-full p-2 mt-2 border border-gray-300 rounded"
+              className="w-full p-2 mt-2 bg-gray-700 text-white border border-gray-600 rounded"
             />
             <div className="search-results mt-2">{searchResultsEnd}</div>
           </div>
+
+          {distance !== undefined && (
+            <div className="mb-4">
+              <label className="block mb-2">Route Information</label>
+              <div className="bg-gray-700 text-white p-2 rounded border border-gray-600">
+                <p>Distance: {distance.toFixed(2)} meters</p>
+              </div>
+            </div>
+          )}
+
+          {steps && (
+            <div className="mb-4">
+              <label className="block mb-2">Instructions</label>
+              <ul className="bg-gray-700 text-white p-2 rounded border border-gray-600 space-y-2">
+                {steps.map((step, index) => (
+                  <li
+                    key={index}
+                    className="flex gap-2 p-2 bg-gray-800 border border-gray-600 rounded cursor-pointer"
+                    onClick={() => console.log(step)}
+                  >
+                    <div className="flex justify-center items-center">
+                      {step.instruction === "Turn left" && (
+                        <ArrowTurnUpLeftIcon className="w-6 h-6" />
+                      )}
+                      {step.instruction === "Turn right" && (
+                        <ArrowTurnUpRightIcon className="w-6 h-6" />
+                      )}
+                      {step.instruction === "Arrive at destination" && (
+                        <MapPinIcon className="w-6 h-6" />
+                      )}
+                    </div>
+                    <div className="flex flex-col">
+                      <p className="text-sm font-semibold">
+                        {step.instruction}
+                      </p>
+                      <p className="text-xs font-normal">
+                        {Math.round(step.distance)} meters
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </aside>
 
         <main className="flex-1 relative">
-          <div ref={elementRef} className="w-full h-[100vh]" />
+          <div ref={elementRef} className="w-full h-full" />
+          <div className="absolute bottom-4 right-4 space-y-2">
+            <button
+              onClick={() => {
+                mapView?.Camera.animate({ zoom: mapView.Camera.zoom - 2000 });
+              }}
+              className="flex justify-center items-center w-8 h-8 shrink-0 bg-gray-700 text-white p-2 rounded-full"
+            >
+              <PlusIcon className="w-6 h-6" />
+            </button>
+            <button
+              onClick={() =>
+                mapView?.Camera.animate({ zoom: mapView.Camera.zoom + 2000 })
+              }
+              className="flex justify-center items-center w-8 h-8 shrink-0 bg-gray-700 text-white p-2 rounded-full"
+            >
+              <MinusIcon className="w-6 h-6" />
+            </button>
+          </div>
         </main>
       </div>
     </section>
